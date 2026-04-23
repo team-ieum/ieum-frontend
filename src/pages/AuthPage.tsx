86c52fabef5c7fb8cde7ca9d21100f259dcbaaa5
sooh329 <glay@kakao.com>
2026-04-23 20:30:33 +0900

    feat: landing 및 auth 페이지 추가

---
+++ src/pages/AuthPage.tsx
@@ -0,0 +1,9 @@
+const AuthPage = () => {
+	return (
+		<section className='space-y-3'>
+			<h1 className='text-2xl font-semibold text-neutral-800'>AuthPage</h1>
+		</section>
+	)
+}
+
+export default AuthPage
