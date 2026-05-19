623807395371868d3d9a8e226cef817410f92949
SeongHwan <epaktldk0203@gmail.com>
2026-05-19 15:52:33 +0900

    design: 워크플로우 채팅 UI 구현

--- src/pages/WorkFlowPage.tsx
+++ src/pages/WorkFlowPage.tsx
@@ -16,6 +16,7 @@ import '@xyflow/react/dist/style.css'
 import { useCallback, useState } from 'react'
 import WorkflowNode, { type WorkflowNodeType } from '@/components/workflow/WorkflowNode'
 import WorkflowToolbar from '@/components/workflow/WorkflowToolbar'
+import WorkflowChat from '@/components/workflow/WorkflowChat'
 
 const nodeTypes = { workflowNode: WorkflowNode }
 
@@ -111,21 +112,24 @@ const WorkFlowPage = () => {
 	return (
 		<div className='-mt-6 -mx-6 -mb-6 lg:-ml-6 flex flex-col' style={{ height: 'calc(100vh - var(--layout-header-height))' }}>
 			<WorkflowToolbar defaultTitle='Lead Qualification Engine' />
-			<style>{`.react-flow__edge.selected .react-flow__edge-path { stroke-width: 3px !important; }`}</style>
-			<ReactFlow
-				nodes={nodes}
-				edges={edges}
-				nodeTypes={nodeTypes}
-				onNodesChange={onNodesChange}
-				onEdgesChange={onEdgesChange}
-				onConnect={onConnect}
-				fitView
-				fitViewOptions={{ padding: 0.2 }}
-			>
-				<Background variant={BackgroundVariant.Dots} gap={18} size={1} color='rgba(125,140,196,.25)' />
-				<Controls />
-				<MiniMap />
-			</ReactFlow>
+			<div className='relative flex-1'>
+				<style>{`.react-flow__edge.selected .react-flow__edge-path { stroke-width: 3px !important; }`}</style>
+				<ReactFlow
+					nodes={nodes}
+					edges={edges}
+					nodeTypes={nodeTypes}
+					onNodesChange={onNodesChange}
+					onEdgesChange={onEdgesChange}
+					onConnect={onConnect}
+					fitView
+					fitViewOptions={{ padding: 0.2 }}
+				>
+					<Background variant={BackgroundVariant.Dots} gap={18} size={1} color='rgba(125,140,196,.25)' />
+					<Controls />
+					<MiniMap />
+				</ReactFlow>
+				<WorkflowChat />
+			</div>
 		</div>
 	)
 }
