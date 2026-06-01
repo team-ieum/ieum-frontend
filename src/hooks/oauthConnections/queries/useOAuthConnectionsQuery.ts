0742338b6a2e40f84a9ab98225001074db10967a
sooh329 <glay@kakao.com>
2026-06-02 01:07:18 +0900

    refactor: 쿼리 반환타입 추가

--- src/hooks/oauthConnections/queries/useOAuthConnectionsQuery.ts
+++ src/hooks/oauthConnections/queries/useOAuthConnectionsQuery.ts
@@ -1,9 +1,10 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, type UseQueryResult } from '@tanstack/react-query'
 import { getOAuthConnections } from '@/api/oauthConnections'
+import type { OAuthConnectionDto } from '@/types/oauthConnections'
 
 export const oauthConnectionsQueryKey = ['oauth-connections', 'list'] as const
 
-export const useOAuthConnectionsQuery = () =>
+export const useOAuthConnectionsQuery = (): UseQueryResult<OAuthConnectionDto[], Error> =>
 	useQuery({
 		queryKey: oauthConnectionsQueryKey,
 		queryFn: getOAuthConnections,
